9ea4597e3eb05031692e1cf8e0845c34928d3eee
yunus-99 <yunuspratmaa19@gmail.com>
2022-10-03 14:25:44 +0700

    Latihan 2

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Yunus</li>
+ <li>Nama Belakang : Pratama</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan ayani 2 komp PIL</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>pontianak</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>musik</li>
+ <li>olahraga</li>
+ </ul>
+</section>
